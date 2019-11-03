bfdcd3811bf99ad795325a546285f819a50df80b
zetraxus <adam.kapica@gmail.com>
2019-11-03 16:48:52 +0100

    #cpp Added reading results from file. Added examples.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include "Graph.h"
 
 typedef std::pair<uint, uint> puu;
@@ -17,14 +18,12 @@ Graph *loadData(std::ifstream &input) {
 }
 
 uint DFSBridge(uint v, int vf, Graph *graph, std::vector<uint> &D, uint cv, std::vector<puu> &bridges) {
-//    std::cout<<"#" << std::endl;
     D[v] = cv;
     uint low = cv++;
-    uint temp;
     for (auto &el : graph->adjacencyMatrix[v]) {
         if (el != vf) {
             if (D[el] == 0) {
-                temp = DFSBridge(el, v, graph, D, cv, bridges);
+                uint temp = DFSBridge(el, v, graph, D, cv, bridges);
                 if (temp < low)
                     low = temp;
             } else {
@@ -34,45 +33,56 @@ uint DFSBridge(uint v, int vf, Graph *graph, std::vector<uint> &D, uint cv, std:
         }
     }
 
-//    std::cout << v << " " << vf << " " << D[v] << " " << low << std::endl;
     if (vf > -1 && low == D[v])
         bridges.emplace_back(std::make_pair(vf, v));
 
     return low;
 }
 
-std::vector<std::pair<uint, uint>> &runAlgorithm(Graph *graph) {
+std::vector<std::pair<uint, uint>> runAlgorithm(Graph *graph) {
     std::vector<puu> bridges;
     std::vector<uint> D(graph->vertices);
     uint cv = 1;
-    uint vf;
 
-    uint start = 1;
     DFSBridge(0, -1, graph, D, cv, bridges);
 
-//    std::cout <<"wynik" <<std::endl;
-    for (auto &el : bridges)
-        std::cout<< el.first << " " << el.second << std::endl;
+    return bridges;
+}
+
+void compareResults(std::ifstream &results, std::vector<puu> &bridges) {
+    uint v1, v2, it = 0;
+    std::sort(bridges.begin(), bridges.end());
+    while (results >> v1 && results >> v2) {
+        if (bridges[it].first != v1 || bridges[it++].second != v2) {
+            std::cout << "failed" << std::endl;
+            return;
+        }
+    }
+    if (it != bridges.size())
+        std::cout << "failed" << std::endl;
+    else
+        std::cout << "success" << std::endl;
 }
 
 int main(int argc, char **argv) {
-    std::ifstream input;
+    std::ifstream data;
+    std::ifstream results;
 
-    if (argc != 2) {
-        std::cout << "Pass filename as argument";
+    if (argc != 3) {
+        std::cout << "Pass input and output files as arguments" << std::endl;
         return -1;
     } else {
-        input.open(argv[1]);
-        if (!input.is_open()) {
-            std::cout << "Can't open file.";
+        data.open(argv[1]);
+        results.open(argv[2]);
+        if (!data.is_open() || !results.is_open()) {
+            std::cout << "Can't open file." << std::endl;
             return -2;
         }
     }
 
-    auto graph = loadData(input);
-    runAlgorithm(graph);
+    auto graph = loadData(data);
+    auto bridges = runAlgorithm(graph);
+    compareResults(results, bridges);
 
     return 0;
 }
-
-//https://eduinf.waw.pl/inf/alg/001_search/0130a.php
