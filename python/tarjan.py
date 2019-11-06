f00da1ea69346667c5d038ce6c3539b5c51762f3
Kamil Mankowski (kam193) <kam193@wp.pl>
2019-11-06 22:50:21 +0100

    Python: code similar to C++, move cleaning

--- python/tarjan.py
+++ python/tarjan.py
@@ -5,11 +5,13 @@ class TarjanAlgorithm(object):
         self._graph = graph  # adjacency list
         self._length = len(graph)
         self._bridges = []
+        self.clean()
 
-    def run(self):
+    def clean(self):
         self._bridges = []
         self._dfs_numbers = [0] * self._length
 
+    def run(self):
         for i in range(self._length):
             if self._dfs_numbers[i] == 0:
                 self._counter = 1
@@ -21,16 +23,17 @@ class TarjanAlgorithm(object):
         self._counter += 1
 
         for u in self._graph[v]:
-            if u == parent:
-                continue
-            if self._dfs_numbers[u] == 0:
-                tmp = self._dfs_tarjan(u, v)
-                if tmp < low:
-                    low = tmp
-            elif self._dfs_numbers[u] < low:
-                low = self._dfs_numbers[u]
+            if u != parent:
+                if self._dfs_numbers[u] == 0:
+                    tmp = self._dfs_tarjan(u, v)
+                    if tmp < low:
+                        low = tmp
+                elif self._dfs_numbers[u] < low:
+                    low = self._dfs_numbers[u]
+
         if parent > -1 and low == self._dfs_numbers[v]:
             self._bridges.append((parent, v))
+
         return low
 
     @property
