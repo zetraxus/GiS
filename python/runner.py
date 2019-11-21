a7139137dd53b43f209b325fcf8b1166e54c95a9
Kamil Mankowski (kam193) <kam193@wp.pl>
2019-11-21 16:15:31 +0100

    Clean Python implementation
    Remove test.sh, add comments and small refactor

--- python/runner.py
+++ python/runner.py
@@ -1,10 +1,31 @@
+""" Module for running Tarjan's algorithm
+
+Algorithm is run with graph from file given as a first
+comand-line argument. Time of execution is printed on
+standard output in microseconds.
+
+Example runs from CLI:
+
+    python3 runner.py graph0.in
+"""
 import sys
 from timeit import default_timer
 
 from tarjan import TarjanAlgorithm
 
 
-def input_reader(input_file):
+def load_graph(input_file):
+    """ Expected file format:
+
+    <vertex_count: int> <edges_count: int>
+    <one edge as a pair u v per line: int int>
+
+    Vertexes' numbers start with 0. Example:
+    4 3
+    0 1
+    1 2
+    2 3
+    """
     with open(input_file, "r") as file:
         v = int(file.readline().split(' ')[0])
         graph = [[] for _ in range(v)]
@@ -25,7 +46,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise Exception("Input file must be specific")
 
-    graph = input_reader(sys.argv[1])
+    # Recursion limit must allow to run recursion DFS
+    # at worst in depth equals to vertex count
+    graph = load_graph(sys.argv[1])
     sys.setrecursionlimit(sys.getrecursionlimit() + len(graph))
     tarjan = TarjanAlgorithm(graph)
 
--- python/tarjan.py
+++ python/tarjan.py
@@ -1,21 +1,26 @@
-
-
 class TarjanAlgorithm(object):
+    """ A Tarjan's Algorithm for bridge finding
+
+    Initialize with graph as adjacency list. After
+    `run`, founded bridges are stored in `bridges` property.
+    """
+    NOT_ANALYZED = 0
+    NO_PARENT = -1
+
     def __init__(self, adjacency_list):
         self._graph = adjacency_list
         self._length = len(adjacency_list)
-        self._bridges = []
         self.clean()
 
     def clean(self):
         self._bridges = []
-        self._dfs_numbers = [0] * self._length
+        self._dfs_numbers = [self.NOT_ANALYZED] * self._length
 
     def run(self):
         for i in range(self._length):
-            if self._dfs_numbers[i] == 0:
+            if self._dfs_numbers[i] == self.NOT_ANALYZED:
                 self._counter = 1
-                self._dfs_tarjan(i, -1)
+                self._dfs_tarjan(i, self.NO_PARENT)
 
     def _dfs_tarjan(self, v, parent):
         self._dfs_numbers[v] = self._counter
@@ -24,14 +29,14 @@ class TarjanAlgorithm(object):
 
         for u in self._graph[v]:
             if u != parent:
-                if self._dfs_numbers[u] == 0:
+                if self._dfs_numbers[u] == self.NOT_ANALYZED:
                     tmp = self._dfs_tarjan(u, v)
                     if tmp < low:
                         low = tmp
                 elif self._dfs_numbers[u] < low:
                     low = self._dfs_numbers[u]
 
-        if parent > -1 and low == self._dfs_numbers[v]:
+        if parent > self.NO_PARENT and low == self._dfs_numbers[v]:
             self._bridges.append((parent, v))
 
         return low
